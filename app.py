b6df04759b3f19a1f032bf8dc95a8850771dceeb
Rarejoe <jb471149@gmail.com>
2026-07-05 23:31:02 +0100

    Fix typo in main check condition

--- app.py
+++ app.py
@@ -297,6 +297,6 @@ def update_status(request_id):
 
 init_db()
 
-if _name_ == "_main_":
+if __name__ == "_main_":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
